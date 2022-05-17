8117e4ef0e887eabfb099967dfee46cf66c5ea73
Alexandra Hedman <hedman.alexandra@gmail.com>
2022-05-17 15:55:51 +0200

    pushing rails task manager with bad css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # list all the tasks
+  get 'tasks', to: 'tasks#index'
 
+  # create a new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # destroy a task
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  # show one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # resources :tasks
 end
